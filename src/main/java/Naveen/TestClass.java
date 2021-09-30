65d3f0a0f3f5dbb60ff7b2ca34f4e8e41a6d0840
mpatel <mpatel@TDS-5CG10762HM>
2021-09-30 11:09:23 +0530

    Added TestClass

--- src/main/java/Naveen/TestClass.java
+++ src/main/java/Naveen/TestClass.java
@@ -4,8 +4,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
-
+		System.out.println("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");
+		
 	}
 
 }
